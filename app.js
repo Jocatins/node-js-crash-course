24ccaf91aaf6919d52b49066f3a221dd9c8a8c35
Nicholas Agwah <nagwah@teqbridgeltd.com>
2023-07-19 13:58:54 +0100

    express

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+// express app
+const app = express();
+
+//listen for requests
+app.listen(4401);
+
+app.get("/", (req, res) => {
+	// res.send("<p>Hello titans</p>");
+	res.sendFile("./views/index.html", { root: __dirname });
+});
+app.get("/about", (req, res) => {
+	res.sendFile("./views/about.html", { root: __dirname });
+});
+
+// redirects
+app.get("/about-us", (req, res) => {
+	res.redirect("/about");
+});
+
+// 404 page
+app.use((req, res) => {
+	res.status(404).sendFile("./views/404.html", { root: __dirname });
+});
